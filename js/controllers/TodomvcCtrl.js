799dc8c18458bc65d575ab4c53931b87589cc143
choijaewoong <jwchoi0129@gmail.com>
2016-06-21 11:38:35 +0900

    todo list 기능 구현 • 컨트롤러에서 todo list 가져오기 • todo list form 에 데이터 연결 • todo list item 삭제 기능 구현

--- js/controllers/TodomvcCtrl.js
+++ js/controllers/TodomvcCtrl.js
@@ -1,6 +1,30 @@
 // 컨트롤러 선언
 angular.module('todomvc')
   .controller('TodomvcCtrl', function($scope){
-    $scope.message = 'Hello World';
+    $scope.todos = [{
+      id:1,
+      title: '청소하기',
+      completed: true
+    },{
+      id:2,
+      title: '코드랩',
+      completed: false
+    },{
+      id:3,
+      title: '귀가',
+      completed: false
+    }
+  ];
 
+  $scope.remove = function(todoId){
+    // console.log('remove() todoId : ' , todoId);
+    // find index
+    var findIndex = $scope.todos.findIndex(function(todo){
+      return todo.id === todoId;
+    });
+    // remove from array
+    if(findIndex > -1){
+      $scope.todos.splice(findIndex, 1);
+    }
+  }
 });
